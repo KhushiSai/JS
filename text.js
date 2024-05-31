ec2f745e49d1088a1a4204f230a039bd00885745
KhushiSai <87372782+KhushiSai@users.noreply.github.com>
2024-05-31 18:02:15 +0000

    basic command

---
+++ text.js
@@ -0,0 +1,37 @@
+/*console.log("khushi")
+const accountId=3652
+let accountEmail="khushisai01@gmail.com"
+var accountPassword="12353"
+accountEmail="hc12@gmail"
+
+console.table([accountEmail,accountId , accountPassword]);
+
+//lec 5 
+"use strict";//treat all js code as newer version
+//alert("plaeae enter your password")//we r using node js not browser 
+let name="khushi" //string
+let age=18//number
+let isLoggedIn=false;//boolean
+//bignit for large no, null , undefined
+let x;
+let state =null;//standlome value 
+//symbol is used to find uniquene component
+//object 
+console.log(typeof  "khushi"); //to find type 
+console.log(typeof null);
+
+
+//conversion 
+let score =33
+console.log(type(score));
+let score ="33"
+let valueInNumber=Number(score)//change
+//"33"=33
+// "33abc"=NaN TRUR=1
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1=true 0=false ""=false "khushi"=true
+
+*/
+
